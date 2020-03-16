8ead9e0b11d45c83b3fed669d86ddd16280926f1
tonyesolomon <slink.sagbe@gmail.com>
2020-03-16 22:31:21 +0000

    Initial commit

---
+++ website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('contact.html', views.contact, name="contact"),
+    path('about.html', views.about, name="about"),
+    path('blog.html', views.blog, name="blog"),
+    # path('blog-details', views.blog-details, name="blog-details"),
+    path('pricing.html', views.pricing, name="pricing"),
+    path('service.html', views.service, name="service"),
+]
---
+++ website/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+def contact(request):
+    if request.method == "POST":
+    	#do stuff
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        # Send mail
+        send_mail(
+        	'Inquiry by:' + message_name,
+        	message,
+        	message_email,
+        	['slink.sagbe@gmail.com'],
+        	fail_silently = False,
+        	)
+
+
+        return render(request, 'contact.html', {'message_name': message_name})
+
+    else:
+
+        return render(request, 'contact.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def blog(request):
+    return render(request, 'blog.html', {})
+
+# def blog-details(request):
+#     return render(request, 'blog-details.html', {})
+
+
+def pricing(request):
+    return render(request, 'pricing.html', {})
+
+
+def service(request):
+    return render(request, 'service.html', {})
